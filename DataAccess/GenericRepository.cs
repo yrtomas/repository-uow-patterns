23292551f1506f68b65c85f8877a1d0abc080a47
yamir_132 <yamir_132@hotmail.com>
2020-11-22 16:11:47 -0300

    Add project files.

---
+++ DataAccess/GenericRepository.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Data;
+
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class GenericRepository<TEntity> where TEntity : class
+    {
+        internal DbContext _context;
+        internal DbSet<TEntity> _dbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public virtual async Task<ICollection<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async virtual Task<IEnumerable<TEntity>> GetAsync(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "")
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+            else
+            {
+                return await query.ToListAsync();
+            }
+        }
+
+        public async virtual Task<TEntity> FindByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async virtual Task<TEntity> Add(TEntity entity)
+        {
+           var result = await  _dbSet.AddAsync(entity);
+
+           return result.Entity;
+        }
+
+        public virtual void DeleteById(object id)
+        {
+            TEntity entityToDelete = _dbSet.Find(id);
+
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            if (_context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entityToDelete);
+            }
+            _dbSet.Remove(entityToDelete);
+        }
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            _dbSet.Attach(entityToUpdate);
+            _context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+        public virtual async Task<TEntity> UpdateAsyn(TEntity t, object key)
+        {
+            if (t == null)
+                return null;
+            TEntity exist = await _context.Set<TEntity>().FindAsync(key);
+            if (exist != null)
+            {
+                _context.Entry(exist).CurrentValues.SetValues(t);
+            }
+            return exist;
+        }
+    }
+}
